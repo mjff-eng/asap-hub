0f4f35e9e41054408ea31ec5aaf4f247317c883e
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-08-25 13:29:47 +0100

    Map neutral300 to its closest CAS colour and drop dark mode from storybook

--- packages/react-components/src/atoms/__tests__/Pill.test.tsx
+++ packages/react-components/src/atoms/__tests__/Pill.test.tsx
@@ -14,8 +14,8 @@ it.each`
   ${'green'}   | ${'rgb(40, 121, 83)'}   | ${'rgb(226, 238, 237)'} | ${'rgb(40, 121, 83)'}
   ${'warning'} | ${'rgb(220, 104, 3)'}   | ${'rgb(252, 248, 238)'} | ${'rgb(220, 104, 3)'}
   ${'info'}    | ${'rgb(12, 141, 195)'}  | ${'rgb(230, 243, 249)'} | ${'rgb(12, 141, 195)'}
-  ${'neutral'} | ${'rgb(136, 147, 154)'} | ${'rgb(250, 250, 250)'} | ${'rgb(136, 147, 154)'}
-  ${'gray'}    | ${'transparent'}        | ${'rgb(250, 250, 250)'} | ${'rgb(86, 96, 102)'}
+  ${'neutral'} | ${'rgb(136, 147, 154)'} | ${'rgb(238, 243, 246)'} | ${'rgb(136, 147, 154)'}
+  ${'gray'}    | ${'transparent'}        | ${'rgb(238, 243, 246)'} | ${'rgb(86, 96, 102)'}
 `(
   'sets text color border and background color for $accent',
   ({ accent, border, text, background }) => {
