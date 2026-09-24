b563ba26959c104eaff8ff6b6ee20c59b3e4f1db
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-09-24 02:32:44 +0100

    deprecate legacy colour names in favour of their cas token

--- packages/react-components/src/colors.ts
+++ packages/react-components/src/colors.ts
@@ -79,51 +79,87 @@ export const colour = {
 };
 
 // Monochrome
+/** @deprecated use `colour.background.secondary`. */
 export const pearl = colour.neutral[25];
+/** @deprecated use `colour.foreground.primary`. */
 export const charcoal = colour.neutral[900];
 
 // Accent
 
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const cerulean = colour.brand.gp2[500];
 
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const space = color(0, 69, 97);
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const azure = colour.brand.gp2[25];
 
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const magenta = color(207, 47, 179);
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const berry = color(154, 35, 134);
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const lilac = colour.general.purple.lavender[25];
 
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const iris = color(140, 78, 159);
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const mauve = color(105, 59, 119);
+/** @deprecated not in CAS; waiting on a design decision (see the CAS colours Notion doc). */
 export const lavender = colour.general.purple.iris[25];
 
+/** @deprecated use `colour.background.error`. */
 export const error100 = colour.utilitarian.red[100];
+/** @deprecated use `colour.foreground.error` for text and icons, `colour.border.error` for borders. */
 export const error500 = colour.utilitarian.red[600];
+/** @deprecated use `colour.background.button.utilitarian.error.hover`. */
 export const error900 = colour.utilitarian.red[700];
 
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const info100 = colour.brand.gp2[25];
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const info150 = colour.brand.gp2[100];
+/** @deprecated a green named info; waiting on a design decision (see the CAS colours Notion doc). */
 export const info200 = colour.brand.crn[100];
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const info500 = colour.brand.gp2[500];
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const info900 = colour.brand.gp2[800];
 
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const information100 = colour.brand.gp2[25];
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const information500 = colour.brand.gp2[500];
+/** @deprecated CAS info is utilitarian blue; waiting on a design decision (see the CAS colours Notion doc). */
 export const information900 = colour.brand.gp2[800];
 
+/** @deprecated closest is `colour.background.secondary` (#FCFCFD instead of #FAFAFA). */
 export const neutral200 = colour.neutral[50];
+/** @deprecated use `colour.background.tertiary`. */
 export const neutral300 = colour.general.blue.cerulean[25];
+/** @deprecated use `colour.border.tertiary` for borders, `colour.background.hover` for backgrounds. */
 export const neutral500 = colour.neutral[100];
+/** @deprecated use `colour.border.secondary`. */
 export const neutral700 = colour.neutral[200];
+/** @deprecated no CAS role; waiting on a design decision (see the CAS colours Notion doc). */
 export const neutral800 = colour.neutral[400];
+/** @deprecated no CAS role for secondary text; waiting on a design decision (see the CAS colours Notion doc). */
 export const neutral900 = colour.neutral[600];
+/** @deprecated use `colour.foreground.primary`. */
 export const neutral1000 = colour.neutral[900];
 
+/** @deprecated CAS success is utilitarian green; waiting on a design decision (see the CAS colours Notion doc). */
 export const success100 = colour.brand.crn[25];
+/** @deprecated CAS success is utilitarian green; waiting on a design decision (see the CAS colours Notion doc). */
 export const success500 = colour.brand.crn[500];
+/** @deprecated CAS success is utilitarian green; waiting on a design decision (see the CAS colours Notion doc). */
 export const success900 = colour.brand.crn[800];
 
+/** @deprecated closest is `colour.background.warning` (orange 100 instead of 50). */
 export const warning100 = colour.utilitarian.orange[50];
+/** @deprecated use `colour.background.warning`. */
 export const warning150 = colour.utilitarian.orange[100];
+/** @deprecated use `colour.foreground.warning` for text and icons, `colour.border.warning` for borders. */
 export const warning500 = colour.utilitarian.orange[600];
+/** @deprecated use `colour.background.button.utilitarian.warning.hover`. */
 export const warning900 = colour.utilitarian.orange[700];
