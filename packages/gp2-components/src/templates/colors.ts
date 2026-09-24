43ece9a26df212d22e63f14e6d955bb968fcc5ee
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-09-24 07:02:53 +0100

    render gp2 stories with the gp2 theme and add a product switch to storybook

--- packages/gp2-components/src/templates/colors.ts
+++
@@ -1,7 +0,0 @@
-import { colorFromHex, colour } from '@asap-hub/react-components';
-
-const colors = {
-  primary500: colorFromHex(colour.brand.gp2[500]),
-  primary900: colorFromHex(colour.brand.gp2[800]),
-};
-export default colors;
