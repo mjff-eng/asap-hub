386d31b3ee516fcaf2d4314f16ebde3c74efd407
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-09-25 02:56:23 +0100

    use the cas colour closest to the original shimmer edges in the loading layout

--- packages/react-components/src/templates/LoadingLayout.tsx
+++ packages/react-components/src/templates/LoadingLayout.tsx
@@ -30,11 +30,13 @@ const animation = (width: string, height: string) =>
         content: '""',
         transform: 'skewX(-45deg)',
         backgroundImage: `linear-gradient(90deg, ${
-          colour.neutral[100]
+          colour.general.blue.classic[25]
         } 0px, ${colourWithAlpha(
           colour.neutral[0],
           0.3,
-        )} calc(${shineWidth}/2), ${colour.neutral[100]} ${shineWidth})`,
+        )} calc(${shineWidth}/2), ${
+          colour.general.blue.classic[25]
+        } ${shineWidth})`,
         position: 'absolute',
         backgroundRepeat: 'repeat-x',
         left: `calc((${width}/2) + ${shineWidth} * -1)`,
