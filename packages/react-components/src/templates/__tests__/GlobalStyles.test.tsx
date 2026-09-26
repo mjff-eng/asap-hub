de7e907967fead41cf3f2ee7cf4df6e324112e58
AimeurAmin <mohammedamin.aimeur@gmail.com>
2026-09-26 03:12:43 +0100

    expect the production brand green in the global styles test

--- packages/react-components/src/templates/__tests__/GlobalStyles.test.tsx
+++ packages/react-components/src/templates/__tests__/GlobalStyles.test.tsx
@@ -15,7 +15,7 @@ it('defines the CAS theme colours as CSS variables for CRN by default', () => {
     getComputedStyle(document.documentElement).getPropertyValue(
       '--colour-background-button-primary-default',
     ),
-  ).toBe('#309466');
+  ).toBe('#34A270');
 });
 
 it('defines the GP2 theme colours when rendered for GP2', () => {
